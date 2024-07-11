1414f684ddc6ecacd044824e42d8682eab8ba47d
Devin B <devinb@Devins-MacBook-Pro.local>
2024-07-11 03:17:16 -0700

    add threejs and related deps. start building out key structure and logic.

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,101 +1,96 @@
+import { acceptedKeys, keyMap } from '#app/constants/keys.js'
 import { type MetaFunction } from '@remix-run/node'
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from '#app/components/ui/tooltip.tsx'
-import { cn } from '#app/utils/misc.tsx'
-import { logos } from './logos/logos.ts'
+import { Canvas, MeshProps } from '@react-three/fiber'
+import { useEffect, useRef, useState } from 'react'
+import { Mesh } from 'three'
+// import SaxKey from '#app/components/ui/saxophone/front-key.js'
 
-export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
+export const meta: MetaFunction = () => [{ title: 'Alto Model' }]
 
-// Tailwind Grid cell classes lookup
-const columnClasses: Record<(typeof logos)[number]['column'], string> = {
-	1: 'xl:col-start-1',
-	2: 'xl:col-start-2',
-	3: 'xl:col-start-3',
-	4: 'xl:col-start-4',
-	5: 'xl:col-start-5',
+interface SaxKeyProps extends MeshProps {
+	hasKeyPearl?: boolean
+	// isPressed?: boolean;
+	keyId: number
+	note: (typeof acceptedKeys)[number] | null
+	// onClick: () => void;
 }
-const rowClasses: Record<(typeof logos)[number]['row'], string> = {
-	1: 'xl:row-start-1',
-	2: 'xl:row-start-2',
-	3: 'xl:row-start-3',
-	4: 'xl:row-start-4',
-	5: 'xl:row-start-5',
-	6: 'xl:row-start-6',
+
+const FrontSaxKey = ({
+	note,
+	hasKeyPearl = false,
+	keyId,
+	...props
+}: SaxKeyProps) => {
+	const meshRef = useRef() as React.MutableRefObject<Mesh>
+	// give each key a number and map as pressed or not based on note
+
+	/** todo: consider user being able to press down sax keys manually, too
+	 *	eg/ keyboard works as saxophone keys
+	 *	eg/ user can click on keys to press them
+	 *	eg/ user can press keys on the screen to press them
+	 */
+	const isPressed =
+		(note === 'a' && (keyId === 1 || keyId === 3)) ||
+		(note === 's' && keyId === 1)
+			? true
+			: false
+
+	return (
+		<mesh ref={meshRef} {...props}>
+			<circleGeometry args={[1, 32]} />
+			<meshBasicMaterial color={isPressed ? 'red' : 'white'} />
+		</mesh>
+	)
 }
 
 export default function Index() {
+	const [note, setNote] = useState<string | null>(null)
+	const [currentOctave, setCurrentOctave] = useState<number>(3)
+
+	useEffect(() => {
+		const handleKeyDown = (e: KeyboardEvent) => {
+			if (acceptedKeys.includes(e.key)) {
+				setNote(e.key)
+				console.log(e.key, note)
+			}
+		}
+
+		document.addEventListener('keydown', handleKeyDown)
+
+		// cleanup, cleanup, everybody do your share
+		return () => document.removeEventListener('keydown', handleKeyDown)
+	}, [])
+
+	const handleOctaveChange = (e: KeyboardEvent) => {
+		if (e.key === 'z' && currentOctave > 1) {
+			setCurrentOctave(currentOctave - 1)
+		} else if (e.key === 'x' && currentOctave < 8) {
+			setCurrentOctave(currentOctave + 1)
+		}
+	}
+
+	useEffect(() => {
+		document.addEventListener('keydown', handleOctaveChange)
+
+		return () => document.removeEventListener('keydown', handleOctaveChange)
+	}, [currentOctave])
+
+	const mappedNote = note && `${keyMap[note]} ${currentOctave}`
 	return (
 		<main className="font-poppins grid h-full place-items-center">
-			<div className="grid place-items-center px-4 py-16 xl:grid-cols-2 xl:gap-24">
-				<div className="flex max-w-md flex-col items-center text-center xl:order-2 xl:items-start xl:text-left">
-					<a
-						href="https://www.epicweb.dev/stack"
-						className="animate-slide-top [animation-fill-mode:backwards] xl:animate-slide-left xl:[animation-delay:0.5s] xl:[animation-fill-mode:backwards]"
-					>
-						<svg
-							className="size-20 text-foreground xl:-mt-4"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 65 65"
-						>
-							<path
-								fill="currentColor"
-								d="M39.445 25.555 37 17.163 65 0 47.821 28l-8.376-2.445Zm-13.89 0L28 17.163 0 0l17.179 28 8.376-2.445Zm13.89 13.89L37 47.837 65 65 47.821 37l-8.376 2.445Zm-13.89 0L28 47.837 0 65l17.179-28 8.376 2.445Z"
-							></path>
-						</svg>
-					</a>
-					<h1
-						data-heading
-						className="mt-8 animate-slide-top text-4xl font-medium text-foreground [animation-delay:0.3s] [animation-fill-mode:backwards] md:text-5xl xl:mt-4 xl:animate-slide-left xl:text-6xl xl:[animation-delay:0.8s] xl:[animation-fill-mode:backwards]"
-					>
-						<a href="https://www.epicweb.dev/stack">The Epic Stack</a>
-					</h1>
-					<p
-						data-paragraph
-						className="mt-6 animate-slide-top text-xl/7 text-muted-foreground [animation-delay:0.8s] [animation-fill-mode:backwards] xl:mt-8 xl:animate-slide-left xl:text-xl/6 xl:leading-10 xl:[animation-delay:1s] xl:[animation-fill-mode:backwards]"
-					>
-						Check the{' '}
-						<a
-							className="underline hover:no-underline"
-							href="https://github.com/epicweb-dev/epic-stack/blob/main/docs/getting-started.md"
-						>
-							Getting Started guide
-						</a>{' '}
-						file for how to get your project off the ground!
-					</p>
-				</div>
-				<ul className="mt-16 flex max-w-3xl flex-wrap justify-center gap-2 sm:gap-4 xl:mt-0 xl:grid xl:grid-flow-col xl:grid-cols-5 xl:grid-rows-6">
-					<TooltipProvider>
-						{logos.map((logo, i) => (
-							<li
-								key={logo.href}
-								className={cn(
-									columnClasses[logo.column],
-									rowClasses[logo.row],
-									'animate-roll-reveal [animation-fill-mode:backwards]',
-								)}
-								style={{ animationDelay: `${i * 0.07}s` }}
-							>
-								<Tooltip>
-									<TooltipTrigger asChild>
-										<a
-											href={logo.href}
-											className="grid size-20 place-items-center rounded-2xl bg-violet-600/10 p-4 transition hover:-rotate-6 hover:bg-violet-600/15 dark:bg-violet-200 dark:hover:bg-violet-100 sm:size-24"
-										>
-											<img src={logo.src} alt="" />
-										</a>
-									</TooltipTrigger>
-									<TooltipContent>{logo.alt}</TooltipContent>
-								</Tooltip>
-							</li>
-						))}
-					</TooltipProvider>
-				</ul>
-			</div>
+			<h1 className="text-center text-4xl font-bold">Welcome to Alto Model!</h1>
+			<p className="text-center text-lg">Play some notes on your keyboard!</p>
+			<span className="text-center text-3xl">
+				{/* {note} */}
+				{mappedNote}
+			</span>
+			<Canvas className="w-96">
+				<spotLight position={[10, 10, 10]} />
+				<ambientLight intensity={0.5} />
+				{Array.from(Array(10).keys()).map((_, i) => (
+					<FrontSaxKey note={note} keyId={i} key={i} position={[0, i - 1, 0]} />
+				))}
+			</Canvas>
 		</main>
 	)
 }
