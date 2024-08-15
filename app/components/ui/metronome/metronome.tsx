677ec1e74cfc413a98a4588074624210e05e4641
Devin B <devin.c.blair@gmail.com>
2024-08-14 18:05:31 -0700

    change input type to slider. update styling. add beat and accent to state.

--- app/components/ui/metronome/metronome.tsx
+++ app/components/ui/metronome/metronome.tsx
@@ -3,15 +3,26 @@ import { Button } from '../button'
 
 const BaseMetronome = () => {
 	const [tempo, setTempo] = useState(60)
+	const [beat, setBeat] = useState(4)
 	const [isPlaying, setIsPlaying] = useState(false)
-	const [selectedMetronomeSample, setSelectedMetronomeSample] =
-		useState('perc_met_0')
+	const [selectedMetronomeSample, setSelectedMetronomeSample] = useState({
+		accent: 'perc_met_1',
+		main: 'perc_met_0',
+	})
 
 	const playMetronomeTick = () => {
-		const tickSound = new Audio(
-			`/samples/metronome/${selectedMetronomeSample}.wav`,
+		const accentTickSound = new Audio(
+			`/samples/metronome/${selectedMetronomeSample?.accent}.wav`,
 		)
-		tickSound.play()
+		const tickSound = new Audio(
+			`/samples/metronome/${selectedMetronomeSample?.main}.wav`,
+		)
+
+		if (beat % 4 === 0) {
+			accentTickSound.play()
+		} else {
+			tickSound.play()
+		}
 	}
 
 	useEffect(() => {
@@ -31,13 +42,17 @@ const BaseMetronome = () => {
 	return (
 		<div className="flex w-full flex-col space-y-6 rounded-xl bg-gray-500 p-8 shadow-md md:w-64">
 			<input
-				className="rounded-md py-2 shadow-md"
-				onChange={(e) => setTempo(Number(e.target.value))}
 				defaultValue={tempo}
+				onChange={(e) => setTempo(Number(e.target.value))}
+				type="range"
+				min="30"
+				max="300"
+				step={1}
 			/>
+			<p className="text-center text-white">{tempo} bpm</p>
 
 			<Button className="shadow-md" onClick={handleIsPlaying}>
-				{isPlaying ? 'stop' : 'start'}
+				{isPlaying ? 'stop' : 'play'}
 			</Button>
 		</div>
 	)
