24ad03ff94c2e870f3ea69828bfe6989f4eb9cce
Devin B <devin.c.blair@gmail.com>
2024-09-06 16:02:43 -0700

    add beat change functionality.

--- app/components/ui/metronome/metronome.tsx
+++ app/components/ui/metronome/metronome.tsx
@@ -11,18 +11,6 @@ const BaseMetronome = () => {
 		main: 'perc_met_0',
 	})
 
-	const playMetronomeTick = () => {
-		const accentTickSound = new Audio(
-			`/samples/metronome/${selectedMetronomeSample?.accent}.wav`,
-		)
-		const tickSound = new Audio(
-			`/samples/metronome/${selectedMetronomeSample?.main}.wav`,
-		)
-
-		// todo: only tickSound playing => figure out what's overriding this
-		currentBeat !== 1 ? tickSound.play() : accentTickSound.play()
-	}
-
 	useEffect(() => {
 		let intervalId: any
 		if (isPlaying) {
@@ -44,6 +32,23 @@ const BaseMetronome = () => {
 		setIsPlaying(!isPlaying)
 	}
 
+	const playMetronomeTick = () => {
+		const accentTickSound = new Audio(
+			`/samples/metronome/${selectedMetronomeSample?.accent}.wav`,
+		)
+		const tickSound = new Audio(
+			`/samples/metronome/${selectedMetronomeSample?.main}.wav`,
+		)
+
+		// todo: only tickSound playing => figure out what's overriding this
+		currentBeat !== 1 ? tickSound.play() : accentTickSound.play()
+	}
+
+	// updates the beat count
+	const handleUpdateBeat = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setBeat(Number(e.target.value))
+	}
+
 	return (
 		<div className="flex w-full flex-col space-y-6 rounded-xl bg-gray-500 p-8 text-center shadow-md md:w-64">
 			<input
@@ -54,9 +59,18 @@ const BaseMetronome = () => {
 				max="300"
 				step={1}
 			/>
-			<div>
+			<div className="space-y-2">
 				<p className="text-white">{tempo} bpm</p>
-				<span className="text-white">{currentBeat}</span>
+				<div className="flex space-x-2">
+					<input
+						defaultValue={4}
+						max={13}
+						min={1}
+						onChange={handleUpdateBeat}
+						type="number"
+					/>
+					<span className="text-white">{currentBeat}</span>
+				</div>
 			</div>
 
 			<Button className="shadow-md" onClick={handleIsPlaying}>
